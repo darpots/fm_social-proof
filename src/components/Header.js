8c85b5d8ec5418e51dc4a3590832ae3f5b1c8530
Darren <darpots@outlook.com>
2020-12-05 21:36:49 +0000

    removed span

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,7 @@ export default function Header({ title, text }) {
     <div className="header">
       <h1>{title}</h1>
       <div>
-        <span>
-          <div>
-            <p>{text}</p>
-          </div>
-        </span>
+        <p>{text}</p>
       </div>
     </div>
   )
